5376b892d99886a58f5d50441507dec61b09e554
SajjaD <SajjadD2557@gmail.com>
2025-08-29 20:16:00 +0330

    Refactor: Apply Facade pattern

--- Patterns/src/main/java/MiniJava/Main.java
+++ Patterns/src/main/java/MiniJava/Main.java
@@ -1,20 +1,12 @@
 package MiniJava;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-import MiniJava.errorHandler.ErrorHandler;
-import MiniJava.parser.Parser;
-
 
 public class Main {
     public static void main(String[] args) {
-        Parser parser = new Parser();
-        try {
-            // start parsing
-            parser.startParse(new Scanner(new File("src/main/resources/code")));
-        } catch (FileNotFoundException e) {
-            ErrorHandler.printError(e.getMessage());
+        CompilerFacade facade = new CompilerFacade();
+        String path = "src/main/resources/code";
+        boolean ok = facade.compileFile(path);
+        if (!ok) {
+            System.exit(1);
         }
     }
 }
