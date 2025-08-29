22db783ac9af0413771af8f3591fc7260f8fc24f
SajjaD <SajjadD2557@gmail.com>
2025-08-29 21:41:32 +0330

    Refactor: Extract Method and Rename Variable applied

--- Patterns/src/main/java/MiniJava/parser/ParseTable.java
+++ Patterns/src/main/java/MiniJava/parser/ParseTable.java
@@ -17,42 +17,60 @@ public class ParseTable {
 
     public ParseTable(String jsonTable) throws Exception {
         jsonTable = jsonTable.substring(2, jsonTable.length() - 2);
-        String[] Rows = jsonTable.split("\\],\\[");
+        String[] rows = jsonTable.split("\\],\\[");
+
         Map<Integer, Token> terminals = new HashMap<>();
         Map<Integer, NonTerminal> nonTerminals = new HashMap<>();
-        Rows[0] = Rows[0].substring(1, Rows[0].length() - 1);
-        String[] cols = Rows[0].split("\",\"");
-        for (int i = 1; i < cols.length; i++) {
-            if (cols[i].startsWith("Goto")) {
-                String temp = cols[i].substring(5);
-                try { nonTerminals.put(i, NonTerminal.valueOf(temp)); }
-                catch (Exception e) { ErrorHandler.printError(e.getMessage()); }
-            } else {
-                terminals.put(i, new Token(Token.getTyepFormString(cols[i]), cols[i]));
-            }
-        }
+
+        processHeader(rows[0], terminals, nonTerminals);
+
         actionTable = new ArrayList<>();
         gotoTable = new ArrayList<>();
-        for (int i = 1; i < Rows.length; i++) {
-            Rows[i] = Rows[i].substring(1, Rows[i].length() - 1);
-            cols = Rows[i].split("\",\"");
-            actionTable.add(new HashMap<>());
-            gotoTable.add(new HashMap<>());
-            for (int j = 1; j < cols.length; j++) {
-                if (!cols[j].equals("")) {
-                    if (cols[j].equals("acc")) {
-                        actionTable.get(actionTable.size() - 1).put(terminals.get(j), new Action(act.accept, 0));
-                    } else if (terminals.containsKey(j)) {
-                        Token t = terminals.get(j);
-                        Action a = new Action(cols[j].charAt(0) == 'r' ? act.reduce : act.shift,
-                                Integer.parseInt(cols[j].substring(1)));
-                        actionTable.get(actionTable.size() - 1).put(t, a);
-                    } else if (nonTerminals.containsKey(j)) {
-                        gotoTable.get(gotoTable.size() - 1).put(nonTerminals.get(j),
-                                Integer.parseInt(cols[j]));
-                    } else {
-                        throw new Exception();
-                    }
+        for (int i = 1; i < rows.length; i++) {
+            processRow(rows[i], terminals, nonTerminals);
+        }
+    }
+
+    private void processHeader(String headerRow, Map<Integer, Token> terminals, Map<Integer, NonTerminal> nonTerminals) {
+        headerRow = headerRow.substring(1, headerRow.length() - 1);
+        String[] columns = headerRow.split("\",\"");
+
+        for (int i = 1; i < columns.length; i++) {
+            if (columns[i].startsWith("Goto")) {
+                String temp = columns[i].substring(5);
+                try {
+                    nonTerminals.put(i, NonTerminal.valueOf(temp));
+                } catch (Exception e) {
+                    ErrorHandler.printError(e.getMessage());
+                }
+            } else {
+                terminals.put(i, new Token(Token.getTyepFormString(columns[i]), columns[i]));
+            }
+        }
+    }
+
+    private void processRow(String row, Map<Integer, Token> terminals, Map<Integer, NonTerminal> nonTerminals) throws Exception {
+        row = row.substring(1, row.length() - 1);
+        String[] columns = row.split("\",\"");
+
+        actionTable.add(new HashMap<>());
+        gotoTable.add(new HashMap<>());
+
+        for (int j = 1; j < columns.length; j++) {
+            if (!columns[j].equals("")) {
+                if (columns[j].equals("acc")) {
+                    actionTable.get(actionTable.size() - 1)
+                            .put(terminals.get(j), new Action(act.accept, 0));
+                } else if (terminals.containsKey(j)) {
+                    Token t = terminals.get(j);
+                    Action a = new Action(columns[j].charAt(0) == 'r' ? act.reduce : act.shift,
+                            Integer.parseInt(columns[j].substring(1)));
+                    actionTable.get(actionTable.size() - 1).put(t, a);
+                } else if (nonTerminals.containsKey(j)) {
+                    gotoTable.get(gotoTable.size() - 1).put(nonTerminals.get(j),
+                            Integer.parseInt(columns[j]));
+                } else {
+                    throw new Exception();
                 }
             }
         }
