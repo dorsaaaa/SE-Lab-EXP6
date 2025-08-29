0771f3092c0b4d307b462c8b09497e1d7bd6f99c
SajjaD <SajjadD2557@gmail.com>
2025-08-29 22:00:35 +0330

    Refactor: Replace magic numbers/strings with constants in Action

--- Patterns/src/main/java/MiniJava/parser/Action.java
+++ Patterns/src/main/java/MiniJava/parser/Action.java
@@ -1,6 +1,10 @@
 package MiniJava.parser;
 
 public class Action {
+    private static final int ACCEPT_NUMBER = 0;
+    private static final char REDUCE_PREFIX = 'r';
+    private static final char SHIFT_PREFIX = 's';
+
     public act action;
     public int number;
 
@@ -13,17 +17,17 @@ public class Action {
     public String toString() {
         switch (action) {
             case accept: return "acc";
-            case shift: return "s" + number;
-            case reduce: return "r" + number;
+            case shift: return SHIFT_PREFIX + number + "";
+            case reduce: return REDUCE_PREFIX + number + "";
         }
         return action.toString() + number;
     }
 
     public static Action fromString(String s) {
-        if (s.equals("acc")) return new Action(act.accept, 0);
+        if (s.equals("acc")) return new Action(act.accept, ACCEPT_NUMBER);
         char type = s.charAt(0);
         int number = Integer.parseInt(s.substring(1));
-        return new Action(type == 'r' ? act.reduce : act.shift, number);
+        return new Action(type == REDUCE_PREFIX ? act.reduce : act.shift, number);
     }
 }
 
